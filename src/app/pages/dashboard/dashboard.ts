78cbe54b4fa984f7a1bc2e013ddfa393e24c6912
Sergio629 <utti232023@utvco.edu.mx>
2026-05-21 08:14:19 -0600

    refactor:se cambio varias cosas

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -45,4 +45,29 @@ export class DashboardComponent {
     'Nuevo mensaje recibido'
   ];
 
+  currentTime = '';
+
+ngOnInit() {
+
+  setInterval(() => {
+
+    const now = new Date();
+
+    this.currentTime = now.toLocaleTimeString();
+
+  }, 1000);
+
+}
+
+days = [
+  1,2,3,4,5,6,7,
+  8,9,10,11,12,13,14,
+  15,16,17,18,19,20,21,
+  22,23,24,25,26,27,28
+];
+
+constructor() {
+  console.log('Hello, World!');
+}
+
 }
