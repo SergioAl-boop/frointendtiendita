f88cfe2ac01e2a7b55686477dab7e839b3bb3833
PVEdJe <edjepeva@gmail.com>
2026-05-26 07:45:54 -0600

    [feat] se agrego products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard';
+import { DashboardComponent } from './pages/products/products';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
 
   },
 
